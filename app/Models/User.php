95a3c7fd1dde06482c6c40962421a2deddbe7591
DahianaMG <122055480+DahianaMG@users.noreply.github.com>
2025-06-06 20:37:39 -0300

    Crea el login

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,18 +3,19 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Role;
+use App\Models\Provincia;
+use App\Models\EstadoTicket;
+use App\Models\TicketComentario;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use App\Models\Provincia;
-use App\Models\Role;
-use App\Models\TicketComentario;
-use App\Models\EstadoTicket;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -52,7 +53,7 @@ class User extends Authenticatable
         ];
     }
 
-     public function provincia()
+    public function provincia()
     {
         return $this->belongsTo(Provincia::class, 'provincia_id', 'id');
     }
--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProvinciaController;
 use App\Http\Controllers\TicketController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\API\AuthController;
 
 //Provincia
-Route::get('get-provincias', [ProvinciaController::class, 'index']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('get-provincias', [ProvinciaController::class, 'index']);
+});
 Route::get('get-provincia-tickets/{id}', [ProvinciaController::class, 'getProvinciaTickets']);
 
 // Tiket
